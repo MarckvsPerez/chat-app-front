cdcdb215eaca50757c6cdceadf7a9fd0096e9325
Marc Pérez <marcantoniperez@gmail.com>
2024-12-31 10:50:50 +0100

    env

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -7,7 +7,7 @@ import { LoginFormData } from "../pages/LoginPage";
 import { UpdateProfileFormData } from "../pages/ProfilePage";
 import { io, Socket } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+const BASE_URL = import.meta.env.VITE_API_URL
 
 type AuthStore = {
     authUser: User | null;
